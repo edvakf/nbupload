6114b81b404e0e133ad79ec14c5f2da967ded56a
Atsushi Takayama <taka.atsushi@gmail.com>
2014-05-24 01:34:35 +0900

    Change to MIT License

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author = "Atsushi Takayama",
     author_email = "taka.atsushi@gmail.com",
     description = ("An IPython Notebook widget for letting users upload a file"),
-    license = "BSD",
+    license = "MIT",
     keywords = "ipython notebook",
     url = "",
     packages=['nbupload'],
